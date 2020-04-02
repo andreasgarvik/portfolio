0f6f72ea84be65c819c4ac970ebc65d17d5c3daa
andreasgarvik <andreas.garvik13@gmail.com>
2020-04-02 18:03:04 +0200

    Moving to flutter :sparkles:

---
+++ lib/src/app.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Text(
+                  'under construction',
+                  style: TextStyle(color: Colors.yellow[600], fontSize: 18),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Icon(
+                  Icons.hourglass_empty,
+                  color: Colors.yellow[600],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
